ed070c0e00df273c8c6e21f83788e801f5fd0458
Cosmin <cosmin.aftanase@yahoo.com>
2021-04-19 21:35:30 +0300

    .

--- hw4/server/index.js
+++ hw4/server/index.js
@@ -1,23 +1,31 @@
 const express = require("express");
-const router = require("./routes");
-const loadMongoose = require("./loaders/loadMongoose");
+// const router = require("./routes");
+// const loadMongoose = require("./loaders/loadMongoose");
 const bodyParser = require("body-parser");
-
-loadMongoose().then((res) => {
-  const app = express();
-  app.use(bodyParser.json());
-
+const app = express();
   app.get('/status', (req, res) => {
     res.json({message: "ok"})
   })
-
-  app.use("/api", router);
-  app.use("/api", (req, res) => {
-    res.status(404).json({ message: "not found" });
-  });
-
   const port = process.env.PORT || 8080;
   app.listen(port, () => {
     console.log(`listening on ${port}`);
   });
-});
+
+// loadMongoose().then((res) => {
+//   const app = express();
+//   app.use(bodyParser.json());
+
+//   app.get('/status', (req, res) => {
+//     res.json({message: "ok"})
+//   })
+
+//   app.use("/api", router);
+//   app.use("/api", (req, res) => {
+//     res.status(404).json({ message: "not found" });
+//   });
+
+//   const port = process.env.PORT || 8080;
+//   app.listen(port, () => {
+//     console.log(`listening on ${port}`);
+//   });
+// });
