3281dd7ee8512a5ca2d5e12360432df95b6f861a
stefanT9 <stefan.tomsa99@gmail.com>
2021-04-27 18:03:03 +0300

    changed api route

--- hw4/functions/reservations/index.js
+++ hw4/functions/reservations/index.js
@@ -1,10 +1,12 @@
 const { default: axios } = require("axios");
 
 module.exports = async function (context, req) {
-    var response = await axios.get(`https://seatr-api.azurewebsites.net/api/reservation`);
-    context.res = {
-        body: {
-            data: [...response.data]
-        }
-    };
-}
+  var response = await axios.get(
+    `https://seatr-backend.azurewebsites.net/api/reservation`
+  );
+  context.res = {
+    body: {
+      data: [...response.data],
+    },
+  };
+};
--- hw4/functions/restaurant/index.js
+++ hw4/functions/restaurant/index.js
@@ -1,13 +1,15 @@
 const { default: axios } = require("axios");
 
 module.exports = async function (context, req) {
-    let qs = "";
-    if (req.query.lat && req.query.lng)
-        qs = `lat=${req.query.lat}&lng=${req.query.lng}`
-    var response = await axios.get(`https://seatr-api.azurewebsites.net/api/restaurant?${qs}`);
-    context.res = {
-        body: {
-            data: [...response.data]
-        }
-    };
-}
+  let qs = "";
+  if (req.query.lat && req.query.lng)
+    qs = `lat=${req.query.lat}&lng=${req.query.lng}`;
+  var response = await axios.get(
+    `https://seatr-backend.azurewebsites.net/api/restaurant?${qs}`
+  );
+  context.res = {
+    body: {
+      data: [...response.data],
+    },
+  };
+};
