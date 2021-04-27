5635ecb1bdbf4cbcdd85e2991a6c80356bf47afd
stefanT9 <stefan.tomsa99@gmail.com>
2021-04-27 18:02:55 +0300

    changed api route

--- hw4/client/src/screens/RestaurantReservations/RestaurantReservations.tsx
+++ hw4/client/src/screens/RestaurantReservations/RestaurantReservations.tsx
@@ -3,7 +3,7 @@ import { Paper, Grid, Typography, Button } from "@material-ui/core";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import Reservation from "../../models/Reservation";
-import axios from 'axios'; 
+import axios from "axios";
 
 interface IProps {
   restaurantName: string;
@@ -14,31 +14,36 @@ const RestaurantReservations = (props: IProps) => {
   const [reservations, setReservations] = useState<Array<Reservation>>([]);
 
   useEffect(() => {
-    axios.get(`/api/reservations`)
-    .then((res) => {
-      setReservations(res.data.data)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+    axios
+      .get(`https://seatr-backend.azurewebsites.net/api/reservations`)
+      .then((res) => {
+        setReservations(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
 
   return (
     <div className="m-2 center-children">
       {reservations.map((val: Reservation, id: number) => (
         <Paper className="w-15 p-t-1 center-children">
-            <Grid>
-              <Typography>restaurant: {val.restaurantName}</Typography>
-              <Typography>phone: {val.userPhone}</Typography>
-              <Typography>date: {val.reservationDate.toString().split("T")[0]}</Typography>
-              <Typography>time: {val.reservationDate.toString().split("T")[1].split('.')[0]}</Typography>
-              <Typography>number of seats:{val.seatCount}</Typography>
-            </Grid>
-            <Grid>
-              <Button> Accept </Button>
-              <Button>Decline</Button>
-            </Grid>
-        </Paper> 
+          <Grid>
+            <Typography>restaurant: {val.restaurantName}</Typography>
+            <Typography>phone: {val.userPhone}</Typography>
+            <Typography>
+              date: {val.reservationDate.toString().split("T")[0]}
+            </Typography>
+            <Typography>
+              time: {val.reservationDate.toString().split("T")[1].split(".")[0]}
+            </Typography>
+            <Typography>number of seats:{val.seatCount}</Typography>
+          </Grid>
+          <Grid>
+            <Button> Accept </Button>
+            <Button>Decline</Button>
+          </Grid>
+        </Paper>
       ))}
     </div>
   );
