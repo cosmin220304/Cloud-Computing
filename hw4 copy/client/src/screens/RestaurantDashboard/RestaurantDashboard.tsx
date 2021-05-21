9d3271c109c7dd4d2b2568773ed82034722f9d86
stefanT9 <stefan.tomsa99@gmail.com>
2021-05-21 23:48:11 +0300

    moved files

--- hw4 copy/client/src/screens/RestaurantDashboard/RestaurantDashboard.tsx
+++ hw4 copy/client/src/screens/RestaurantDashboard/RestaurantDashboard.tsx
@@ -1,4 +1,9 @@
 import React from "react";
 
-const RestaurantDashboard = () => {};
+const RestaurantDashboard = () => {
+
+    return <div>
+        
+    </div>
+};
 export default RestaurantDashboard;
