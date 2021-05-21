a8baa96d26ad58f18b67613761699a3a3db34317
Stefan <cosmin.aftanase@yahoo.com>
2021-05-22 00:58:24 +0300

    removed functions

--- seatr/client/src/setupProxy.js
+++ seatr/client/src/setupProxy.js
@@ -4,5 +4,10 @@ module.exports = function(app) {
   app.use(createProxyMiddleware("/api", { 
     target: "https://seatr-the-best-api.azurewebsites.net",
     changeOrigin: true,
+    secure: false,
+    headers: {
+      host: "mytestbackend.azurewebsites.net"
+    },
+    cookieDomainRewrite: ""
   }));
 };
