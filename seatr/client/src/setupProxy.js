05b62fada79a5662f97fa063130bc9dc8d179f8c
Cosmin <cosmin.aftanase@yahoo.com>
2021-05-22 13:03:33 +0300

    repaired npm and proxy

--- seatr/client/src/setupProxy.js
+++ seatr/client/src/setupProxy.js
@@ -1,11 +1,13 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
+const url = process.env.NODE_ENV === "development" ? "http://localhost:8080" : "https://seatr-the-best-api.azurewebsites.net";
 module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:8080",
+      target: url,
       changeOrigin: true,
+      secure: false,
     })
   );
 };
